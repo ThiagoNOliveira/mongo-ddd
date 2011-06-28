b12feb7af67cb5c571908800fc279e5a628905dd
Nathan Bridgewater <nathan.bridgewater@adventuretechgroup.com>
2011-06-28 10:51:57 -0500

    test

---
+++ src/Site.Cmd/Domain/SiteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Site.Cmd.Domain
+{
+	public class SiteRepository
+	{
+		public void AddDomainToSite(Site site, DomainName domain)
+		{
+		}
+	}
+}
